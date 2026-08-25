7f29e66238a46df3486e449288e9ea4d66c33d47
Manoj <manojullagaddi.msu@gmail.com>
2026-08-25 16:19:45 +0530

    Add files via upload

---
+++ Object_Oriented_Programming(OOP)/Anonymous_Object1.java
@@ -0,0 +1,12 @@
+
+class Greeting {
+    void printCustomeMessage() {
+        System.out.println("Welcome to Java Programming");
+    }
+}
+
+public class Anonymous_Object1 {
+    public static void main(String[] args) {
+        new Greeting().printCustomeMessage();
+    }
+}
